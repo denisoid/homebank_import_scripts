1d1e8feaf818a1b120ce20bcbcc124274ac2c43a
Denis Oleshkevich <denis@web-dev.org>
2011-09-17 10:18:28 +0400

    Added proccesing with several accounts

--- moneyiq_convert.py
+++ moneyiq_convert.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-import sys, sqlite3, time
+import sys, sqlite3, time, os.path
 
 
 if len(sys.argv) != 2:
@@ -11,19 +11,24 @@ if len(sys.argv) != 2:
 #get filename
 filename = sys.argv[1]
 
+#connect to database
 conn = sqlite3.connect(filename)
 
 #query to get all expenses and categories names
 query="""select * from expenses
 left join categories on (expenses.category_id = categories.id)
+where expenses.deleted = 0; """
 
-"""
+
+
 
 #make cursor and load data to it
 conn.row_factory=sqlite3.Row
 cur=conn.cursor()
 cur.execute(query)
 
+#expenses divide to several dicts
+accounts={}
 
 #enumaration expenses
 for row in cur:
@@ -56,5 +61,32 @@ for row in cur:
 	
 	#glue list and print it
 	line=u";".join(out)
-	print line.encode('utf-8')
 	
+	account_id=row["account_id"];
+	
+	if not account_id in accounts:
+		accounts[account_id]=[]
+	
+	
+	accounts[account_id].append(line.encode('utf-8'))
+	#print line.encode('utf-8')
+	
+	
+#saving to csv
+
+basename=os.path.splitext(filename)[0];
+
+	
+for account_id in accounts:
+	account_filename=basename + "_" + `account_id` + ".csv";
+	filedata="\n".join(accounts[account_id]);
+	
+
+	
+	
+	fileObj = open(account_filename,"w")
+	fileObj.write(filedata);
+	fileObj.close()
+	
+	print account_id
+#print accounts;
