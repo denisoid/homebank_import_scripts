3e99ee32b480369f368684a47ace76103b7bf969
Fran Quinto <1702904+fquinto@users.noreply.github.com>
2022-09-03 22:11:51 +0200

    New version 1.0.3 from year 2018 improve category

--- AEB43_Q43/q43_TO_csv.py
+++ AEB43_Q43/q43_TO_csv.py
@@ -3,11 +3,15 @@
 
 # Author   Version  Date        Comments
 # FQuinto  1.0.0    2011-05-21  First Release
-# FQuinto  1.0.1    2016-12-27  New version for Homebank v.5.0.9 and for Python v.3.x
+# FQuinto  1.0.1    2016-12-27  For Homebank v.5.0.9 and Python 3
 # FQuinto  1.0.2    2017-01-01  New output qif
+# FQuinto  1.0.3    2018-01-20  Remove string, improve category
+
+# TODO
+# Improve auto set TAGs
 
 # Special for Caixa d'Enginyers (https://www.caixa-enginyers.com)
-# Copyright (C) 2011-2016 Fran Quinto
+# Copyright (C) 2011-2018 Fran Quinto
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -21,8 +25,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import string
 import sys
+import datetime
+import locale
 
 # Archivo de entrada
 filename_Q43 = sys.argv[1]
@@ -46,18 +51,18 @@ category = ''
 for line in fileIN_Q43.readlines():
     if line[0:2] == '11':
         cuenta = line[2:18]
-        date_inicio_volcado = line[20:22] + '/' + line[22:24] + '/20' + line[18:20]
-        date_final_volcado = line[26:28] + '/' + line[28:30] + '/20' + line[24:26]
-
-        fileOUT_CSV.write('date;paymode;info;payee;wording;amount;category;tags' + '\n')
-
+        date_inicio_volcado = (line[20:22] + '/' + line[22:24] +
+                               '/20' + line[18:20])
+        date_final_volcado = (line[26:28] + '/' + line[28:30] +
+                              '/20' + line[24:26])
+        header = 'date;paymode;info;payee;memo;amount;category;tags'
+        fileOUT_CSV.write(header + '\n')
         if qif_output:
             fileOUT_QIF.write('!Account\n' +
                               'N' + cuenta + '\n' +
                               'TOth A\n' +
                               '^\n' +
                               '!Type:Oth A\n')
-
     if line[0:2] == '22':
         # data operacio
         anyo = line[10:12]
@@ -82,6 +87,9 @@ for line in fileIN_Q43.readlines():
         maxlinechars = 60
         info = line[4:maxlinechars].strip()
 
+        # Default tags
+        tags = ''
+
         # set payee
         payee = ''  # default
         payeeNu = ['*4016', 'NURIA LAGO', 'Nuria Lago']
@@ -102,7 +110,7 @@ for line in fileIN_Q43.readlines():
             category = 'variable:efectivo'
         elif 'INGRES EFECTIU' in info:
             category = 'variable:efectivo'
-        elif (string.find(info, 'NOMINA')) != -1:
+        elif info.find('NOMINA') != -1:
             category = 'fijo:entrada'
         elif info[0:5] == 'TRASP':
             if signo == '-':
@@ -128,23 +136,34 @@ for line in fileIN_Q43.readlines():
             category = '-------ERROR--------'
 
         # Casa:impuestos
-        categoryArray = ['OALGT', 'MUSA', 'FINCAS CARBONELL']
+        categoryArray = ['OALGT', 'MUSA']
         if any(txt in info for txt in categoryArray):
             category = 'Casa:impuestos'
+
+        # Casa:comunidad
+        categoryArray = ['FINCAS CARBONELL', 'COM. DE']
+        if any(txt in info for txt in categoryArray):
+            category = 'Casa:comunidad'
+
         # Casa:hipoteca
         categoryArray = ['QUOTA', 'CUOTA']
         if any(txt in info for txt in categoryArray):
             category = 'Casa:hipoteca'
+            tags = 'HIPOTECA'
+
         # Banco:bolsa
         categoryArray = ['ADEUDO VALORES', 'DERECHOS', 'CUPONES']
         if any(txt in info for txt in categoryArray):
             category = 'Banco:bolsa'
+
         # Banco:comisiones
         categoryArray = ['COM. CAIXER', 'COMIS']
         if any(txt in info for txt in categoryArray):
             category = 'Banco:comisiones'
+
         # Banco:otros
-        categoryArray = ['ABONO PRES', 'ADEUDO PROVISION', 'ADEUDO TITULO', 'ABONAMENT', 'INTERES', 'BONIFICACIO']
+        categoryArray = (['ABONO PRES', 'ADEUDO PROVISION', 'ADEUDO TITULO',
+                         'ABONAMENT', 'INTERES', 'BONIFICACIO'])
         if any(txt in info for txt in categoryArray):
             category = 'Banco:otros'
 
@@ -154,29 +173,58 @@ for line in fileIN_Q43.readlines():
             category = 'Casa:movil'
 
         # Casa:telefono
-        categoryArray = ['TELEFONICA', 'ORANGE']
+        categoryArray = (['TELEFONICA', 'ORANGE', 'JAZZTEL',
+                         'Adamo Telecom Iberia S.A.'])
+        categoryArrayLOW = [x.lower() for x in categoryArray]
+        categoryArrayUP = [x.upper() for x in categoryArray]
+        categoryArrayCAP = [x.capitalize() for x in categoryArray]
         if any(txt in info for txt in categoryArray):
             category = 'Casa:telefono'
+            tags = 'GASTOS VIVIENDA'
+        elif any(txt in info for txt in categoryArrayLOW):
+            category = 'Casa:telefono'
+            tags = 'GASTOS VIVIENDA'
+        elif any(txt in info for txt in categoryArrayUP):
+            category = 'Casa:telefono'
+            tags = 'GASTOS VIVIENDA'
+        elif any(txt in info for txt in categoryArrayCAP):
+            category = 'Casa:telefono'
+            tags = 'GASTOS VIVIENDA'
+
         # Casa:electricidad
-        categoryArray = ['ENDESA ENERGIA']
+        categoryArray = ['ENDESA ENERGIA', 'ENDESA ENER ENDES']
         if any(txt in info for txt in categoryArray):
             category = 'Casa:electricidad'
+            tags = 'GASTOS VIVIENDA'
+
         # Casa:agua
-        categoryArray = ['AIGUES DE BARCELONA']
+        categoryArray = (['AIGUES DE BARCELONA', 'SOCIEDAD GENERAL DE AGUA',
+                         'SOCIEDAD GENERAL DE SGAB'])
         if any(txt in info for txt in categoryArray):
             category = 'Casa:agua'
+            tags = 'GASTOS VIVIENDA'
+
         # Casa:gas
         categoryArray = ['Gas Natural', 'GAS NATURAL']
         if any(txt in info for txt in categoryArray):
             category = 'Casa:gas'
+            tags = 'GASTOS VIVIENDA'
+
         # Seguros
-        categoryArray = ['VIDA', 'SEGURO']
+        categoryArray = (['VIDA', 'SEGURO',
+                         'CAJA INGENIEROS, OBS VINCULADO, SLU'])
         if any(txt in info for txt in categoryArray):
             category = 'Seguros'
+
         # Casa:deco
-        categoryArray = ['IKEA', 'LEROY']
+        categoryArray = (
+            ['IKEA', 'LEROY', 'AMAZON.ES COMPRA AMAZO',
+             'MUY MUCHO BARNASUD', 'JARDI GAVA S A',
+             'AMAZON *MKTPLCE', 'AMAZON.ES COMPRA', 'BAUHAUS BARCELONA']
+        )
         if any(txt in info for txt in categoryArray):
             category = 'Casa:deco'
+
         # Casa:parking
         categoryArray = ['JOSEL S.L.U.']
         if any(txt in info for txt in categoryArray):
@@ -193,17 +241,43 @@ for line in fileIN_Q43.readlines():
             category = 'Ocio:alojamiento'
 
         # Casa:comida
-        categoryArray = ['Markt', 'MARKT', 'MARCHE', 'EROSKI', 'CARREFOURMARKET', 'ALDI S JOAN DESPI TDA 30', 'EMBUTIDOS LA MASIA', 'BON AREA', 'BON PREU SAU', '02 MAKRO BARCEL', 'MERCADONA', 'CARREFOUR', 'CAPRABO', 'LIDL']
+        categoryArray = (
+            ['Markt', 'MARKT', 'MARCHE', 'EROSKI', 'CARREFOURMARKET',
+             'ALDI S JOAN DESPI TDA 30', 'EMBUTIDOS LA MASIA', 'BON AREA',
+             'BON PREU SAU', '02 MAKRO BARCEL', 'MERCADONA', 'CARREFOUR',
+             'CAPRABO', 'LIDL', 'ALCAMPO SANT BOI', 'Amazon *Mktplce EU-ES',
+             'AMAZON SPAIN RETAIL AM', 'BO FRESC', 'PRIME NOW MKTPL',
+             'Prime Now Mktplc ES', 'Amazon Prime Now ES', 'CASA AMETLLER'])
+        categoryArrayLOW = [x.lower() for x in categoryArray]
+        categoryArrayUP = [x.upper() for x in categoryArray]
         if any(txt in info for txt in categoryArray):
+            category = 'Casa:comida'
+        elif (any(txt in info for txt in categoryArray)):
+            category = 'Casa:comida'
+        elif (any(txt in info for txt in categoryArrayUP)):
             category = 'Casa:comida'
 
         # Ocio:restaurante
-        categoryArray = ['Restaurant', 'STARBUCKS', 'McDonald\'s', 'CERVECERIA MORITZ', 'RESTAURANT', 'MCDONALDS', 'NOSTRUM', 'ENRIQUE TOMAS', 'TOMMY MEL\'S', 'RESTAURANTE', 'ES ALAR DEL REY', 'CACHON SL', 'CUYNES SCP', 'EINSTEIN AM BIKINI PS GM', 'Kaisers', 'PINTXOS SELECTOS', 'PANS LA ROCA', 'LA MARQUESINA', 'PEIXOS ELS BOMBA', 'CUINATS', 'TIO CRISTOBAL', 'LA LLOSA', 'FOSTERS HOLLYWOOD', 'Buon appetito', 'maravillas', 'EL BUEN TAPEOCASA', 'VIENA']
+        categoryArray = (
+            ['Restaurant', 'STARBUCKS', 'McDonald\'s', 'CERVECERIA MORITZ',
+             'RESTAURANT', 'MCDONALDS', 'NOSTRUM', 'ENRIQUE TOMAS',
+             'TOMMY MEL\'S', 'RESTAURANTE', 'ES ALAR DEL REY', 'CACHON SL',
+             'CUYNES SCP', 'EINSTEIN AM BIKINI PS GM', 'Kaisers',
+             'PINTXOS SELECTOS', 'PANS LA ROCA', 'LA MARQUESINA',
+             'PEIXOS ELS BOMBA', 'CUINATS', 'TIO CRISTOBAL', 'LA LLOSA',
+             'FOSTERS HOLLYWOOD', 'Buon appetito', 'maravillas',
+             'EL BUEN TAPEOCASA', 'VIENA', 'PIZZERIA CLAUDIUS',
+             'AIRE PALAU FALGUERA', 'CUINES STA. CATERINA',
+             'ESPAI PLA A MIREIA', 'RTE. JAPONES SAMURAI',
+             'EL PINTXO DE PETRITXOL', 'TOCCA RISTORANTE PIZZERI',
+             'BURGUER KING CASTELDEFEL'])
         if any(txt in info for txt in categoryArray):
             category = 'Ocio:restaurante'
 
         # Ocio:billetes
-        categoryPeajes = ['FENE', 'GUISAMO', 'A.C.E.S.A', 'S/AP15', 'AP6', 'A8/A1', 'INVICAT', 'AUTOPISTA', 'ARTEIXO', 'LA RIBERA']
+        categoryPeajes = (
+            ['FENE', 'GUISAMO', 'A.C.E.S.A', 'S/AP15', 'AP6', 'A8/A1',
+             'INVICAT', 'AUTOPISTA', 'ARTEIXO', 'LA RIBERA'])
         if any(txt in info for txt in categoryPeajes):
             category = 'Ocio:billetes'
         categoryBici = ['Reisebuero Im Europa Cen']
@@ -212,10 +286,13 @@ for line in fileIN_Q43.readlines():
         categoryMetro = ['RATP', 'METRO BARCELONA']
         if any(txt in info for txt in categoryMetro):
             category = 'Ocio:billetes'
+        categoryCoche = ['TAXI 82 ELCHE-ALICANTE']
+        if any(txt in info for txt in categoryCoche):
+            category = 'Ocio:billetes'
         categoryTren = ['RENFE', 'SNCF']
         if any(txt in info for txt in categoryTren):
             category = 'Ocio:billetes'
-        categoryAvion = ['IBERIA', 'RYANAIR']
+        categoryAvion = ['IBERIA', 'RYANAIR', 'VUELING AIRLINES']
         if any(txt in info for txt in categoryAvion):
             category = 'Ocio:billetes'
         categoryBarco = ['EMBARCACIO']
@@ -229,7 +306,8 @@ for line in fileIN_Q43.readlines():
         categoryArray = ['DECATHLON']
         if any(txt in info for txt in categoryArray):
             category = 'Ocio:deporte'
-        categoryArray = ['WONDERFUL', 'PEPPA PAPER', 'PPPO DEU I MATA', 'PAPYRUS']
+        categoryArray = (['WONDERFUL', 'PEPPA PAPER', 'PPPO DEU I MATA',
+                         'PAPYRUS', 'LAIE COSMOCAIXA LLIBRERI'])
         if any(txt in info for txt in categoryArray):
             category = 'Ocio:papeleria'
         categoryArray = ['promobo']
@@ -240,7 +318,7 @@ for line in fileIN_Q43.readlines():
         categoryMuseos = ['museum', 'MUSE']
         if any(txt in info for txt in categoryMuseos):
             category = 'Ocio:experience'
-        categoryBalneario = ['SEMTEE SA WWW']
+        categoryBalneario = ['SEMTEE SA WWW', 'BALNEARIOS2X1.CO']
         if any(txt in info for txt in categoryBalneario):
             category = 'Ocio:experience'
         categoryCine = ['MULTICINES']
@@ -248,75 +326,46 @@ for line in fileIN_Q43.readlines():
             category = 'Ocio:experience'
 
         # paymode
-        if info[0:14] == 'INGRESO CHEQUE':
+        # default
+        paymode = '0'  # nada
+
+        paymode2 = (['INGRESO CHEQUE', 'ADEUDO CHEQUE'])
+        if any(txt in info for txt in paymode2):
             paymode = '2'  # cheque
-        elif info[0:13] == 'ADEUDO CHEQUE':
-            paymode = '2'  # cheque
-        elif info[0:6] == 'EFECTI':
-            paymode = '3'  # efectivo
-        elif info[0:16] == 'INGRESO EFECTIVO':
-            paymode = '3'  # efectivo
-        elif info[0:14] == 'INGRES EFECTIU':
-            paymode = '3'  # efectivo
-        elif info[0:14] == 'CAIXER TARG. *':
+
+        paymode3 = (['EFECTI', 'INGRESO EFECTIVO', 'INGRES EFECTIU',
+                    'CAIXER TARG. *'])
+        if any(txt in info for txt in paymode3):
             paymode = '3'  # efectivo
             category = 'variable:efectivo'
-        elif info[0:6] == 'TRANSF':
-            paymode = '4'  # transferencia (-)
-        elif info[0:10] == 'DEVOLUCION':
-            paymode = '4'  # transferencia (-)
-        elif (string.find(info, 'NOMINA')) != -1:
-            paymode = '4'  # transferencia (-)
-        elif info[0:5] == 'TRASP':
+
+        paymode4 = (['TRANSF', 'DEVOLUCION', 'TRASP', 'REINTEGRO', 'NOMINA'])
+        if any(txt in info for txt in paymode4):
             paymode = '4'  # transferencia
-        elif info[0:4] == 'paymode':
-            paymode = '4'  # transferencia
-        elif info[0:9] == 'REINTEGRO':
-            paymode = '4'  # transferencia
-        elif info[0:5] == 'DEBIT':
+
+        paymode6 = (['ANUL.TARGETA', 'DEBIT', 'ANUL.LACIO DEBIT',
+                    'ANULACION DEBITO', 'TARGETA *', 'DEVOLUCIO'])
+        if any(txt in info for txt in paymode6):
             paymode = '6'  # tarjeta de debito
-        elif info[0:16] == 'ANUL.LACIO DEBIT':
-            paymode = '6'  # tarjeta de debito
-        elif info[0:16] == 'ANULACION DEBITO':
-            paymode = '6'  # tarjeta de debito
-        elif info[0:9] == 'TARGETA *':
-            paymode = '6'  # tarjeta de debito
-        elif info[0:9] == 'DEVOLUCIO':
-            paymode = '6'  # tarjeta de debito
-        elif info[0:7] == 'CUPONES':
+
+        paymode7 = (['CUPONES', 'DERECHOS', 'ADEUDO VALORES'])
+        if any(txt in info for txt in paymode7):
             paymode = '7'  # orden de posicion
-        elif info[0:8] == 'DERECHOS':
-            paymode = '7'  # orden de posicion
-        elif info[0:14] == 'ADEUDO VALORES':
-            paymode = '7'  # orden de posicion
-        elif info[0:2] == 'R/':
+
+        paymode8 = (['R/', 'CARGA MOVILES', 'SEGURO', 'ASSEG. CE VIDA'])
+        if any(txt in info for txt in paymode8):
             paymode = '8'  # paymode electronico
-        elif info[0:13] == 'CARGA MOVILES':
-            paymode = '8'  # paymode electronico
-        elif info[0:6] == 'SEGURO':
-            paymode = '8'  # paymode electronico
-        elif info[0:5] == 'QUOTA':
+
+        paymode9 = (['QUOTA', 'CUOTA'])
+        if any(txt in info for txt in paymode9):
             paymode = '9'  # deposito
-        elif info[0:5] == 'CUOTA':
-            paymode = '9'  # deposito
-        elif info[0:11] == 'BONIFICACIO':
+
+        paymode10 = (
+            ['BONIFICACIO', 'COMIS', 'INTERES', 'ABONAMENT', 'ADEUDO TITULO',
+             'ADEUDO PROVISION', 'ABONO PRES',
+             'COM. CAIXER'])
+        if any(txt in info for txt in paymode10):
             paymode = '10'  # honorarios
-        elif info[0:5] == 'COMIS':
-            paymode = '10'  # honorarios
-        elif info[0:7] == 'INTERES':
-            paymode = '10'  # honorarios
-        elif info[0:9] == 'ABONAMENT':
-            paymode = '10'  # honorarios
-        elif info[0:13] == 'ADEUDO TITULO':
-            paymode = '10'  # honorarios
-        elif info[0:16] == 'ADEUDO PROVISION':
-            paymode = '10'  # honorarios
-        elif info[0:10] == 'ABONO PRES':
-            paymode = '10'  # honorarios
-        elif info[0:11] == 'COM. CAIXER':
-            paymode = '10'  # honorarios
-        else:
-            paymode = '0'  # nada
 
         # paymode
         # 0 nada
@@ -329,22 +378,44 @@ for line in fileIN_Q43.readlines():
         # 7 Orden de posicion
         # 8 Pago electronico
         # 9 Deposito
-        # 10 Honorarios
+        # 10 Honorarios FI
+        # 11 Cargo directo
         # FIN paymode
         # date;paymode;info;payee;wording;amount;category;tags
+
+        # Homebank 5.1.7
+        # date;paymode;info;payee;memo;amount;category;tags
+        # From wording to memo
         # info = string.replace(info, ':', '')
-        print(info)
-        print('   category ' + category)
-        print('   payee ' + payee)
+
         # set homebank to %d/%m/%Y
         # default inside homebank is %x
         date = dia + '/' + mes + '/' + anyo
-        wording = ''
+        memo = ''
         amount = signo + real + '.' + decimal
-        tags = ''
-        fileOUT_CSV.write(date + ';' + paymode + ';' + info + ';' + payee + ';' + wording + ';' + amount + ';' + category + ';' + tags + '\n')
+
+        # Get weekday name (year, month, day)
+        loc = locale.getlocale()
+        locale.setlocale(locale.LC_ALL, loc)
+        # 'es_ES.UTF-8'
+        mydate = datetime.date(int('20' + anyo), int(mes), int(dia))
+
+        # Print only if error
+        if ('-------ERROR--------' == category) or ('0' == paymode):
+            print(info)
+            print('   category ' + category)
+            print('   payee ' + payee)
+            print('   amount ' + amount)
+            print('   date ' + mydate.strftime("%A") + ' ' + date)
+            print('   paymode ' + paymode)
+
+        fileOUT_CSV.write(date + ';' + paymode + ';' + info + ';' + payee +
+                          ';' + memo + ';' + amount + ';' + category +
+                          ';' + tags + '\n')
         if qif_output:
-            fileOUT_QIF.write('D' + date + '\nT' + amount + '\nC\nP' + payee + '\nM' + wording + '\nL' + category + '\n^\n')
+            fileOUT_QIF.write('D' + date + '\nT' + amount + '\nC\nP' +
+                              payee + '\nM' + memo + '\nL' +
+                              category + '\n^\n')
 
 fileIN_Q43.close()
 fileOUT_CSV.close()
